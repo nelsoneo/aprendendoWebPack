6e4abf95aa16d27fbba4bebbe4d3687f48a79bc6
nelsoneo <nelsoneo@gmail.com>
2021-09-17 22:33:28 -0300

    variaveis

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+const buttomA = <button>Historico de clientes</button>
+
+const hasCustomer = false
+
 const App = () => {
   return (
     <div>
       <p>Digital Invovation one</p>
       <p>Bem-vindo a nosso primeiro Exercies</p>
+      {hasCustomer && (
+        <div>
+        {buttomA}
+      </div>
+      )}
     </div>
   );
 };

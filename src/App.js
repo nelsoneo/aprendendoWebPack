2fbd31783202c0bdde5be645bc9dd01d4fe8dd03
nelsoneo <nelsoneo@gmail.com>
2021-09-17 23:17:52 -0300

    ajustado

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
 import React from 'react';
 
+
 const buttomA = <button>Historico de clientes</button>
 const buttomB = <button>Cadastrar clientes</button>
 
-const hasCustomer = true
+const hasCustomer = true;
 
 const App = () => {
 
-  const renderShowHistorico = () => {
+
+const ShowHistorico = () => (
     <div>
     Cliclar no botão abaixo
       <br />
       {buttomA}
   </div>
-}
+)
 
-const renderAddCustomer = () => {
+const AddCustomer = () => (
     <div>
       Cliclar no botão Cadastrar Clintes
       <br />
       {buttomB}
   </div>
+)
+
+const renderShowCustomer = () => {
+  if (!hasCustomer) return null
+
+  return (
+    <div>
+        <h1>Nome do cliente: Nelson HG</h1>
+    </div>
+  )
 }
 
   return (
     <div>
       <p>Digital Invovation one</p>
       <p>Bem-vindo a nosso primeiro Exercies</p>
-      {hasCustomer ? renderShowHistorico() : renderAddCustomer()}
-      {/* {hasCustomer ? renderShowHistorico() : renderAddCustomer()} */}
+      {hasCustomer ? ShowHistorico() : AddCustomer()}
+      <div>
+        {renderShowCustomer()}
+      </div>
     </div>
   );
 };

0ad2e46fd770805fd1a145747bf585cfbc32e0c6
nelsoneo <nelsoneo@gmail.com>
2021-09-17 22:39:17 -0300

    if inline

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 
 const buttomA = <button>Historico de clientes</button>
+const buttomB = <button>Cadastrar clientes</button>
 
-const hasCustomer = false
+const hasCustomer = true
 
 const App = () => {
   return (
     <div>
       <p>Digital Invovation one</p>
       <p>Bem-vindo a nosso primeiro Exercies</p>
-      {hasCustomer && (
+      {hasCustomer ? (
         <div>
+          <p>Cliclar no botão abaixo</p>
+          <br />
         {buttomA}
       </div>
+      ) : (
+        <div>
+          <p>Cliclar no botão Cadastrar Clintes</p>
+          <br />
+          {buttomB}
+        </div>
       )}
     </div>
   );
